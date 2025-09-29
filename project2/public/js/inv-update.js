794ea53cfd11ece1fb86e6f0e339b03ecf0a7e35
Avee1000 <eddyidahosa01@gmail.com>
2025-09-29 12:28:05 +0100

    Move project1 to root and remove project2

--- project2/public/js/inv-update.js
+++
@@ -1,5 +0,0 @@
-const form = document.querySelector("form")
-form.addEventListener("change", function () {
-    const updateBtn = document.querySelector("button")
-    updateBtn.removeAttribute("disabled")
-})
